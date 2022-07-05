eb00fffbc33ff4b60657fc95d639dbd9c5ded10b
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-05 00:23:32 -0300

    🚚 views: shared header + footer

---
+++ Projeto-Empresarial/resources/views/shared/header.blade.php
@@ -0,0 +1,23 @@
+<header class="bg-indigo-500">
+    <div class="container mx-auto flex justify-between p-5 items-center">
+        <a href="#" class="flex title-font font-medium items-center text-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-indigo-500 p-2 bg-white rounded-full"
+                viewBox="0 0 24 24">
+                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-xl text-white">My store</span>
+        </a>
+        <div class="flex items-center">
+
+            <a href="#"
+                class="inline-flex items-center bg-white border-0 py-1 px-3 focus:outline-none hover:bg-gray-100 rounded text-indigo-500 md:mt-0">
+                Admin
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+            </a>
+        </div>
+    </div>
+</header>
