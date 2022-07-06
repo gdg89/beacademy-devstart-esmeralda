ed68bf819c5c265452793448b0fcf09e87ec2dfc
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-05 23:50:40 -0300

    🔥 remove user edit + delete routes

--- Projeto-Empresarial/app/Http/Controllers/UserController.php
+++ Projeto-Empresarial/app/Http/Controllers/UserController.php
@@ -48,38 +48,4 @@ class UserController extends Controller
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
-    }
 }
