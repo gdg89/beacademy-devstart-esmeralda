5484d4c2bad1ec99b39a44e61ae97ddaf9a2123a
Giuliano  García <91767060+gdg89@users.noreply.github.com>
2022-07-07 18:49:13 -0300

    Revert "add login.blade.php e newAccess no controller"

--- Projeto-Empresarial/resources/views/layouts/default.blade.php
+++ Projeto-Empresarial/resources/views/layouts/default.blade.php
@@ -4,23 +4,14 @@
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-
     @vite(['resources/js/app.js', 'resources/css/app.css'])
-    @stack('styles')
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="EstanteDev" />
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>@yield('title') | EstanteDev</title>
 </head>
 
-<body>
-
+<body class="flex flex-col min-h-screen">
     @include('shared.header')
 
     <main class="grow">
