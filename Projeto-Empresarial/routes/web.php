a848348504fbf399247373f3a3f59d9e5c5822c2
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-06 23:48:16 -0300

    update routes

--- Projeto-Empresarial/routes/web.php
+++ Projeto-Empresarial/routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\{
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/produto/{id}', [ProductController::class, 'show'])->name('product');
+Route::get('/produto/{product}', [ProductController::class, 'show'])->name('product.show');
 
 Route::prefix('usuario')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('user.index');
@@ -19,37 +19,38 @@ Route::prefix('usuario')->group(function () {
     Route::get('/login', [UserController::class, 'login'])->name('user.login');
     Route::get('/logoff', [UserController::class, 'logoff'])->name('user.logoff');
 
-    Route::post('/request', [UserController::class, 'request'])->name('user.request');
-
     Route::get('/cadastro', [UserController::class, 'create'])->name('user.create');
     Route::post('/cadastro', [UserController::class, 'store'])->name('user.store');
-
-    Route::get('/editar', [UserController::class, 'edit'])->name('user.edit');
-    Route::post('/editar', [UserController::class, 'update'])->name('user.update');
-
-    Route::post('/delete/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 
     Route::get('/carrinho', [ClientController::class, 'cart'])->name('user.cart');
     Route::post('/carrinho', [ClientController::class, 'finish'])->name('user.index');
 });
 
-Route::prefix('admin/produtos')->group(function () {
-    Route::get('/', [AdminProductController::class, 'index'])->name('admin.products.index');
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminProductController::class, 'index'])->name('admin.index');
 
-    Route::get('/cadastro', [AdminProductController::class, 'create'])->name('admin.products.create');
-    Route::post('/cadastro', [AdminProductController::class, 'store'])->name('admin.products.store');
+    Route::prefix('produto')->group(function () {
+        Route::get('/cadastro', [AdminProductController::class, 'create'])->name('admin.product.create');
+        Route::post('/cadastro', [AdminProductController::class, 'store'])->name('admin.product.store');
 
-    Route::get('/editar/{id}', [AdminProductController::class, 'edit'])->name('admin.products.edit');
-    Route::post('/editar/{id}', [AdminProductController::class, 'update'])->name('admin.products.update');
+        Route::get('/editar/{product}', [AdminProductController::class, 'edit'])->name('admin.product.edit');
+        Route::put('/editar/{product}', [AdminProductController::class, 'update'])->name('admin.product.update');
 
-    Route::post('/delete/{id}', [AdminProductController::class, 'destroy'])->name('admin.products.destroy');
+        Route::delete('/delete/{product}', [AdminProductController::class, 'destroy'])->name('admin.product.destroy');
+
+        Route::get('/delete/image/{product}', [AdminProductController::class, 'destroyImage'])->name('admin.product.destroy.image');
+    });
 });
 
-Route::prefix('/pedidos')->group(function () {
+Route::prefix('pedidos')->group(function () {
     Route::get('/', [OrderController::class, 'index'])->name('orders.index');
 
     Route::get('/cadastro', [OrderController::class, 'create'])->name('orders.create');
     Route::post('/cadastro', [OrderController::class, 'store'])->name('orders.store');
+});
+
+Route::prefix('admin/pedidos')->group(function () {
+    Route::get('/', [OrderController::class, 'index'])->name('admin.orders.index');
 
     Route::get('/editar/{id}', [OrderController::class, 'edit'])->name('orders.edit');
     Route::post('/editar/{id}', [OrderControllerer::class, 'update'])->name('orders.update');
