6814a7d5c0e70fb55d5a20708641f4c978996ad8
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-08 01:12:08 -0300

    ✨ Admin Orders List

--- Projeto-Empresarial/app/Http/Controllers/AdminOrderController.php
+++ Projeto-Empresarial/app/Http/Controllers/AdminOrderController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminOrderController extends Controller
 {
     public function index()
     {
-        $orders = Order::all();
+        $orders = Order::paginate(5);
 
         // dd($orders->toArray());
+
+        foreach ($orders as $order) {
+            $order->quantity = $order->products->count();
+
+            $order->cost = $order->products->sum('price_cost');
+            $order->total = $order->products->sum('price_sell');
+            $order->profit = $order->total - $order->cost;
+        }
+
+        foreach ($orders as $order) {
+            $order->cost = Product::format_price($order->cost);
+            $order->total = Product::format_price($order->total);
+            $order->profit = Product::format_price($order->profit);
+        }
 
         return view('admin.orders.index', compact('orders'));
     }
--- Projeto-Empresarial/resources/views/admin/orders/index.blade.php
+++ Projeto-Empresarial/resources/views/admin/orders/index.blade.php
@@ -1,6 +1,97 @@
 @extends('layouts.default')
-@section('title', 'Home')
-
+@section('title', 'Admin Pedidos')
 @section('content')
-<h1>order index</h1>
+
+<section class="text-gray-600 py-16">
+
+    {{-- @include('shared.search') --}}
+
+    <div class="container px-5 py-2 mx-auto w-full">
+        {{ $orders->links() }}
+    </div>
+
+    <div class="container px-5 pt-8 pb-24 mx-auto w-full">
+
+
+        <div class="w-full mx-auto overflow-auto">
+            <div class="flex items-center justify-between mb-2">
+                <h1 class="text-2xl font-medium title-font mb-2 text-gray-900">
+                    Pedidos
+                </h1>
+                <a href="#" class="flex ml-auto text-white bg-indigo-500 border-0 py-1.5 px-3 text-sm focus:outline-none
+                hover:bg-indigo-600 rounded">
+                    Adicionar
+                </a>
+            </div>
+            <table class="table-auto w-full text-left whitespace-no-wrap">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                            #
+                        </th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                            Qtd
+                        </th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                            Total (R$)
+                        </th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                            Custo (R$)
+                        </th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                            Lucro (R$)
+                        </th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                            Email do Cliente
+                        </th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                            Status
+                        </th>
+                        <th
+                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 text-right">
+                            Ações
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y">
+
+                    @foreach ($orders as $order)
+                    <tr class="even:bg-gray-100 odd:bg-white">
+                        <td class="px-4 py-3">{{ $order->id }}</td>
+                        <td class="px-4 py-3">{{ $order->quantity }}</td>
+                        <td class="px-4 py-3">{{ $order->total }}</td>
+                        <td class="px-4 py-3">{{ $order->cost }}</td>
+                        <td class="px-4 py-3">{{ $order->profit }}</td>
+                        <td class="px-4 py-3">{{ $order->user->email }}</td>
+                        <td class="px-4 py-3 h-full">
+
+                            <span class="
+                                px-3 py-1
+                                inline-flex items-center justify-center
+                                rounded-full text-sm font-medium
+                                @if ($order->status == 'Processando')
+                                    bg-yellow-200 text-yellow-800
+                                @elseif ($order->status == 'Recusado')
+                                    bg-red-200 text-red-700
+                                @elseif ($order->status == 'Aprovado')
+                                    bg-green-200 text-green-800
+                                @else
+                                    bg-indigo-200 text-indigo-800
+                                @endif
+                            ">
+                                {{ $order->status }}
+                            </span>
+                        </td>
+                        <td class="px-4 py-3 text-sm text-right space-x-3">
+                            <a href="#" class="text-indigo-500 inline-flex items-center">Visualizar</a>
+                        </td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+
 @endsection
