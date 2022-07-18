e5218efd636b545256766212177dd42e872375ab
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-18 19:23:35 -0300

    💄 styles: orders index table view

--- Projeto-Empresarial/resources/views/admin/orders/index.blade.php
+++ Projeto-Empresarial/resources/views/admin/orders/index.blade.php
@@ -39,7 +39,7 @@
                         Status
                     </th>
 
-                    <th class="table-th text-center">
+                    <th class="table-th text-right">
                         Ações
                     </th>
                 </tr>
@@ -55,17 +55,11 @@
                     <td class="px-4 py-3">
                         @include('shared.status')
                     </td>
-                    <td class="px-4 py-3 text-sm text-center">
+                    <td class="px-4 py-3 text-sm text-right">
                         <a href="{{ route('admin.orders.show', $order->id) }}"
                             title="Visualizar pedido {{ $order->id }}"
                             class="text-indigo-500 hover:text-indigo-600 inline-flex items-center justify-center">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                                <path fill-rule="evenodd"
-                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
+                            Visualizar
                         </a>
                     </td>
                 </tr>
