bd56fab6744db961d633192d9bcd30a549653b52
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-24 19:16:28 -0300

    🎨 Update  Views: default + home-links

--- Projeto-Empresarial/resources/views/layouts/default.blade.php
+++ Projeto-Empresarial/resources/views/layouts/default.blade.php
@@ -4,16 +4,18 @@
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.1/dist/flowbite.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 
     @vite(['resources/js/app.js', 'resources/css/app.css'])
-    @stack('styles')
 
+    @stack('styles')
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.1/dist/flowbite.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
     <meta name="description" content="EstanteDev" />
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>@yield('title') | EstanteDev</title>
