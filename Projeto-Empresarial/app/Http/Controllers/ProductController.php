e428d0d3007e2a14f6c9c87a9b277751e804fbf0
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-25 01:07:49 -0300

    ♻️ refactor: Home Controller to Product Controller

--- Projeto-Empresarial/app/Http/Controllers/ProductController.php
+++ Projeto-Empresarial/app/Http/Controllers/ProductController.php
@@ -7,8 +7,32 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function index(Request $request)
+    {
+        $products = Product::query();
+
+        $products->when($request->search, function ($query, $search) {
+            return $query->where('name', 'like', "%{$search}%");
+        });
+
+        $products = $products->paginate(12);
+
+        foreach ($products as $product) {
+            $product->price_sell = Product::format_price($product->price_sell);
+            $product->cover = Product::getProductCoverPath($product);
+        }
+
+        if ($request->search) {
+            $products->appends('search', $request->search);
+        }
+
+        return view('product.index', [
+            'products' => $products
+        ]);
+    }
+
     /**
-     * Display the specified resource.
+     * Display single product.
      *
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
