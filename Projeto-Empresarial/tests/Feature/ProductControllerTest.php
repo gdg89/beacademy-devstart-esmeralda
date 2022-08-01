1922ad2280e989932c1bbe040f679f62dc2eddb7
Giuliano <giuilianogarcia2826@gmail.com>
2022-08-01 17:58:24 -0300

    Tests AdminProductController

---
+++ Projeto-Empresarial/tests/Feature/ProductControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_admin_access_product_create()
+    {   
+         $user= User::factory()->create();
+
+        $response = $this->post('/login',[
+            'email'=> $user['email'],
+            'password'=> $user['password'],
+        ]);
+        
+        $this->actingAs($user);
+        $response = $this->get('/admin/produto/cadastro');
+
+        $response->assertStatus(200);
+    }
+    
+    public function test_product_store()
+    {   
+        $user = User::factory()->create();
+
+        $response = $this->post('/login',[
+            'email'=> $user['email'],
+            'password'=> $user['password'],
+        ]);
+        
+        $this->actingAs($user);
+        
+        
+        $product = Product::factory()->create();
+        $response= $this->post('/admin/produto/cadastro', [
+            'name'=> $product['name'],
+            'slug'=> $product['slug'],
+            'price_cost'=> $product['price_cost'],
+            'price_sell'=> $product['price_sell'],
+            'cover'=> $product['cover'],
+            'stock'=> $product['stock'],
+            'description'=> $product['description']
+        ])->assertRedirect('/');
+        
+        
+
+    }
+    
+    
+}
---
+++ Projeto-Empresarial/tests/Unit/ProductControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_admin_access_create_product()
+    {
+        $this->assertTrue(true);
+    }
+    
+    public function test_product_store()
+    {
+        $this->assertTrue(true);
+    }
+}
