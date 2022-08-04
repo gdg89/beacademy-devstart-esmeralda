d476f0202d479404724587903c3fa07aee6bda3d
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-04 15:29:23 -0300

    🐛 fix: reset DB config

--- Projeto-Empresarial/config/database.php
+++ Projeto-Empresarial/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DATABASE_HOST'),
-            'port' => env('DATABASE_PORT'),
-            'database' => env('DATABASE_NAME'),
-            'username' => env('DATABASE_USER'),
-            'password' => env('DATABASE_PASS'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'prefix_indexes' => true,
             'search_path' => 'public',
             'charset' => 'utf8',
