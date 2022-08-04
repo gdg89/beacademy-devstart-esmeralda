b580cabe89b30c8556205b4b0d13b6d2e7c13ce8
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-04 17:38:07 -0300

    🐛 fix: remove app.css from default view

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -18,7 +18,7 @@ $cartPaths = [
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js'])
 
     @stack('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
