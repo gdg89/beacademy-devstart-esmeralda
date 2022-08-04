da6d2a9622259b195c195485e899edebc8d09efa
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-04 18:18:17 -0300

    🗃️ create user admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+        User::factory()->create([
+            'isAdmin' => true,
+            'name' => 'Luís V. Capelletto',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('123123'),
+            'email_verified_at' => now(),
+            'avatar' => 'https://res.cloudinary.com/capelaum/image/upload/v1648581498/admin-uploads/xgeusezbgvtxuit2fn5e.jpg',
+            'birthday' => '1996-01-01',
+            'phone' => '(11) 99999-9999',
+            'cpf' => '12312312311',
+            'cep' => '12345-123',
+            'state' => 'Distrito Federal',
+            'city' => 'CIDADE',
+            'district' => 'BAIRRO',
+            'street' => 'ENDEREÇO',
+            'complement' => 'Complemento',
+            'number' => '123',
+        ]);
+
         User::factory(20)->create();
     }
 }
